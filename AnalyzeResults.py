739614bab5e11480dfe62f3a927c6e16a9777048
Selin Bac <68256746+SelinBac@users.noreply.github.com>
2022-02-14 15:49:27 -0800

    Add files via upload

--- AnalyzeResults.py
+++ AnalyzeResults.py
@@ -19,35 +19,51 @@ from matplotlib.lines import Line2D
 from scipy.io import loadmat
 import ZCT
 
-T_tunnel=[300]
-print('Temperature:',T_tunnel[0],' K')
-
 filename_true = 'CF3CH3_dft2svp_step50_disp5000_num100.pkl'
+#filename_true = 'CH4_dft2svp_step50_disp5000_num100_reversed.pkl'
+#filename_true = 'C2H6_dft2svp_step50_disp5000_num100_reversed.pkl'
+#filename_true = 'Ir_dft2svp_step50_disp5000_num100_reversed.pkl'
+#filename_true = 'MADH-od2_dft2svp_step50_disp5000-extend-odd_num100_reversed.pkl'
+#filename_true = 'Sn2C1_dft2svp_step100_disp5000_num100.pkl'
+#filename_true = 'Sn2Ar1_dft2svp_step100_disp5000_num100.pkl'
+#filename_true = 'Sn2Ar5_dft2svp_step100_disp5000_num100.pkl'
+#filename_true = 'Sn2Ar6_dft2svp_step100_disp5000_num100.pkl'
 
 filename = 'resultsT_All_ntrials100_CF3CH3_dft2svp_step50_disp5000_num100_samptypeGuaranteeAllRows_density0.6.pkl' 
+#filename = 'resultsT_All_ntrials100_CH4_dft2svp_step50_disp5000_num100_samptypeGuaranteeAllRows_density0.35.pkl'
+#filename = 'resultsT_All_ntrials100_C2H6_dft2svp_step50_disp5000_num100_samptypeGuaranteeAllRows_density0.3.pkl'
+#filename = 'resultsT_All_ntrials100_Ir_dft2svp_step50_disp5000_num100_samptypeGuaranteeAllRows_density0.5.pkl'
+#filename = 'resultsT_All_ntrials100_MADH-od2_dft2svp_step50_disp5000-exten-o_num100_samptypeGuaranteeAllRows_density0.1.pkl'
+#filename = 'resultsT_All_ntrials100_Sn2C1_dft2svp_step100_disp5000_num100_samptypeGuaranteeAllRows_density0.2.pkl'
+#filename = 'resultsT_All_ntrials100_Sn2Ar1_dft2svp_step100_disp5000_num100_samptypeGuaranteeAllRows_density0.05.pkl'
+#filename = 'resultsT_All_ntrials100_Sn2Ar5_dft2svp_step100_disp5000_num100_samptypeGuaranteeAllRows_density0.045.pkl'
+#filename = 'resultsT_All_ntrials100_Sn2Ar6_dft2svp_step100_disp5000_num100_samptypeGuaranteeAllRows_density0.045.pkl'
+
 
 print(filename)
-alldf_zct = pd.read_pickle(filename_true)
-mirror=False #true if only half of the MEP is available
+alldf_true = pd.read_pickle(filename_true)
 
-if mirror:   
-    df_copy=alldf_zct.copy()
-    df_copy=df_copy.drop(0)
-    df_copy.index = -1 * df_copy.index
-    df_copy['s'] = -1 *df_copy['s']
-    df_copy=df_copy.sort_index(ascending=True)
-    alldf_zct=df_copy.append(alldf_zct)
+T_tunnel=[300]
+calc_ZCT = False #True activates tunneling calculations
+mirror = False #true if only half of the MEP is available
+use_trueVAGandE0 = True #false if trial VAG and E0 wanted in ZCT calculations
 
-s=alldf_zct['s']
-s_list=[]
-for i in s.index:
-    s_list.append(s[i])
-evals=alldf_zct['Eigenvalues']
-Vmep_zct=alldf_zct['Energy'] 
-index_zct=alldf_zct.index
-
-ZCT_true,E0_true,VAG_true,SAG_true=ZCT.zct(T_tunnel,index_zct,evals,Vmep_zct,s_list,s,'calc','calc','calc')
-print('True ZCT:',ZCT_true)
+s=alldf_true['s']
+if calc_ZCT:  
+    print('Temperature:',T_tunnel[0],' K')
+    if mirror:   
+        df_copy=alldf_true.copy()
+        df_copy=df_copy.drop(0)
+        df_copy.index = -1 * df_copy.index
+        df_copy['s'] = -1 *df_copy['s']
+        df_copy=df_copy.sort_index(ascending=True)
+        alldf_true=df_copy.append(alldf_true)
+    
+    evals=alldf_true['Eigenvalues']
+    Vmep=alldf_true['Energy'] 
+    
+    ZCT_true,E0_true,VAG_true,SAG_true,V_aG_true=ZCT.zct(T_tunnel,evals,Vmep,s,'calc','calc','calc')
+    print('True ZCT:',ZCT_true)
 
 def get_eigenvalue_matrix(df,tag='All'):
     evals = []
@@ -283,7 +299,7 @@ for index, row in alldf.iterrows():
     all_colerr_init.append(colerr_init)
     all_colerr_vmc.append(colerr_vmc)
 
-print(filename)
+#print(filename)
 #raise Exception("This is just to get out, comment me when unneeded")
 thermodf = pd.DataFrame(thermodict)
 
@@ -356,6 +372,7 @@ GaccFreqMean = count_acc(alldf['G error, mean, 1000K'],accuracy)
 ZPEaccFreqMean = count_acc(alldf['ZPE error, mean'],accuracy)
 
 plt.savefig('Figures/'+'MeanError_'+system+sample_type+density+number_trials+'.png')
+print('\n# Trials:',ntrials)
 print("#Trials with maximum error within chemical accuracy: ")
 print('ZPE: ', ZPEaccFreqMax)
 print('Delta G, 1000K: ', GaccFreqMax)
@@ -365,7 +382,7 @@ print('ZPE: ', ZPEaccFreqMean)
 print('Delta G, 1000K: ', GaccFreqMean)
 
 
-print("Average initial error (%):", "{:.2f}".format(np.mean(error_init)*100.))
+print("\nAverage initial error (%):", "{:.2f}".format(np.mean(error_init)*100.))
 print("Average VMC error (%):", "{:.2f} +- {:.2f}".format(np.mean(error_final)*100.,np.std(error_final)*100.))
 
 
@@ -405,8 +422,8 @@ for i in range(len(G_max)):
     if G_max[i]>accuracy:
         count_G_max+=1    
     
-print('DeltaG_mean - within chemical accuracy (%)',(GaccFreqMean/ntrials)*100) 
-print('DeltaG_max - within chemical accuracy (%)',(GaccFreqMax/ntrials)*100) 
+#print('DeltaG_mean - within chemical accuracy (%)',(GaccFreqMean/ntrials)*100) 
+#print('DeltaG_max - within chemical accuracy (%)',(GaccFreqMax/ntrials)*100) 
 
 xlow = min(Trueval)-2
 xhigh = max(Trueval)+5
@@ -449,10 +466,8 @@ for i in range(len(ZPE_max)):
     if ZPE_max[i]>accuracy:
         count_ZPE_max+=1
 
-#print('ZPE_mean - within chemical accuracy (%)',(1-count_ZPE_mean/ntrials)*100)
-#print('ZPE_max - within chemical accuracy (%)',(1-count_ZPE_max/ntrials)*100)    
-print('ZPE_mean - within chemical accuracy (%)',(ZPEaccFreqMean/ntrials)*100)
-print('ZPE_max - within chemical accuracy (%)',(ZPEaccFreqMax/ntrials)*100) 
+#print('ZPE_mean - within chemical accuracy (%)',(ZPEaccFreqMean/ntrials)*100)
+#print('ZPE_max - within chemical accuracy (%)',(ZPEaccFreqMax/ntrials)*100) 
 
 
 deltaG_max=alldf['G error, max, 1000K']
@@ -462,82 +477,211 @@ min_err=min(abs(deltaG_max))
 
 for i in range(len(deltaG_max)):
     if deltaG_max[i]==max_err:
-        print('maximum',i,deltaG_max[i])
+        print('maximum deltaG error (index,value):',i,deltaG_max[i])
     elif deltaG_max[i]==-max_err:
-        print('maximum',i,deltaG_max[i])
+        print('maximum deltaG error (index,value):',i,deltaG_max[i])
     elif deltaG_max[i]==min_err:
-        print('minimum',i,deltaG_max[i])
+        print('minimum deltaG error (index,value):',i,deltaG_max[i])
     elif deltaG_max[i]==-min_err:
-        print('minimum',i,deltaG_max[i])
-        
-print('average',(max_err+min_err)/2)
+        print('minimum deltaG error (index,value):',i,deltaG_max[i])
+avg_deltaG=min(deltaG_max, key=lambda x:abs(x-np.mean(abs(deltaG_max))))
+print('average deltaG error (index,value)',list(deltaG_max).index(avg_deltaG),avg_deltaG,'\n')
+
+
+
 #ZCT Calculations  
-
-if mirror:
-    for i in range(ntrials):
-        copy=ZPE_all[i]
-        copy.remove(copy[0])
-        copy=copy[::-1]
-        ZPE_all[i]=copy+ZPE_all[i]
+if calc_ZCT:
         
-ZCT_err=[]
-ZCT_all=[]
-E0_list=[]
-VAG_list=[]
-SAG_list=[]
-
-#repeats_array = np.tile(index_zct, (len(alldf),1))
-#alldf.insert(2,'index_zct',repeats_array)
-for index, row in alldf.iterrows():
-    dict_zct=[]
     if mirror:
+        for i in range(ntrials):
+            copy=ZPE_all[i]
+            copy.remove(copy[0])
+            copy=copy[::-1]
+            ZPE_all[i]=copy+ZPE_all[i]
             
-        evals_trial=row['Xfinal'].T
-        evals_trial=evals_trial.tolist()
-        evals_copy=evals_trial.copy()
-        evals_trial.remove(evals_trial[0])
-        evals_trial=evals_trial[::-1]
-        evals_mirrored=np.array(evals_trial+evals_copy)
-        
-        
-            
-    for i in range(len(np.array(index_zct))):
+    ZCT_err=[]
+    ZCT_all=[]
+    E0_list=[]
+    VAG_list=[]
+    SAG_list=[]
+    V_aG_list=[]
+    
+    for index, row in alldf.iterrows():
+        dict_zct=[]
         if mirror:
-            MEP=np.array(Vmep_zct)
-            dict_zct.append({'index_zct':np.array(index_zct)[i],'Vmep':MEP[i],'evals':evals_mirrored[i]})
+            evals_trial=row['Xfinal'].T
+            evals_trial=evals_trial.tolist()
+            evals_copy=evals_trial.copy()
+            evals_trial.remove(evals_trial[0])
+            evals_trial=evals_trial[::-1]
+            evals_mirrored=np.array(evals_trial+evals_copy)
+                
+        for i in range(len(np.array(s.index))):
+            if mirror:
+                dict_zct.append({'index_zct':np.array(s.index)[i],'Vmep':np.array(Vmep)[i],'evals':evals_mirrored[i]})
+            else:
+                dict_zct.append({'index_zct':np.array(s.index)[i],'Vmep':row['Energies'][i],'evals':row['Xfinal'].T[i]})
+        df_zct = pd.DataFrame.from_dict(dict_zct)    
+        df_zct = df_zct.set_index('index_zct')   
         
+        
+        if use_trueVAGandE0:
+            # use True E0 and VAG
+            ZCT_trial,E0_trial,VAG_trial,SAG_trial,V_aG_trial=ZCT.zct(T_tunnel,df_zct['evals'],df_zct['Vmep'],s,E0_true,VAG_true,SAG_true)
         else:
-            dict_zct.append({'index_zct':np.array(index_zct)[i],'Vmep':row['Energies'][i],'evals':row['Xfinal'].T[i]})
-    df_zct = pd.DataFrame.from_dict(dict_zct)    
-    df_zct = df_zct.set_index('index_zct')   
+            # use calculated (Trial) data
+            ZCT_trial,E0_trial,VAG_trial,SAG_trial,V_aG_trial=ZCT.zct(T_tunnel,df_zct['evals'],df_zct['Vmep'],s,'calc','calc','calc')
+        
+        ZCT_all.append(ZCT_trial)
+        ZCT_err.append(abs(ZCT_trial-ZCT_true)/ZCT_true*100)
+        E0_list.append(E0_trial)
+        VAG_list.append(VAG_trial)
+        SAG_list.append(SAG_trial)
+        V_aG_list.append(V_aG_trial)
+        if index%10==0:
+                print('Trial #',index,'ZCT:',ZCT_trial)
+    
+    plt.figure(0)
+    plt.hist(ZCT_err)
+    plt.xlabel('$\kappa^{SAG}$ Error (%)')
+    plt.ylabel('Number of Trials')
     
     
-   
-    # use True E0 and VAG
-    ZCT_trial,E0_trial,VAG_trial,SAG_trial=ZCT.zct(T_tunnel,index_zct,df_zct['evals'],df_zct['Vmep'],s_list,s,E0_true,VAG_true,SAG_true)
-   
-    # use calculated (Trial) data
-    #ZCT_trial,E0_trial,VAG_trial,SAG_trial=ZCT.zct(T_tunnel,index_zct,df_zct['evals'],df_zct['Vmep'],s_list,s,'calc','calc','calc')
+    plt.savefig('ResultsZCT/'+'ZCT_hist_'+system+'_'+sample_type+'_'+density+'_'+number_trials+'_T_'+str(T_tunnel[0])+'.png')
     
-    ZCT_all.append(ZCT_trial)
-    ZCT_err.append(abs(ZCT_trial-ZCT_true)/ZCT_true*100)
-    E0_list.append(E0_trial)
-    VAG_list.append(VAG_trial)
-    SAG_list.append(SAG_trial)
-    #dict_zct.append({'ZCT_true':ZCT_true,'ZCT_trial':ZCT_trial,'ZCT_err':ZCT_err,'E0_true':E0_true,'E0_trial',E0_trial,'VAG_true':VAG_true,'VAG_trial',VAG_trial,'SAG_true':SAG_true,'SAG_trial',SAG_trial})
-    #print('Trial #',index,'\nZCT:',ZCT_trial,'\nE0:',E0_trial)
-    print('Trial #',index,'\nZCT:',ZCT_trial)
+    new_dict=[]
+    if use_trueVAGandE0:
+        new_dict.append({'ZCT_true':ZCT_true,'E0_true':E0_true,'VAG_true':VAG_true,'V_aG_true':V_aG_true,'SAG_true':SAG_true,'ZCT_trials_based_on_trueE0andVAG':np.array(ZCT_all),'ZCT_err':ZCT_err,'E0_trials':E0_list,'VAG_trials':VAG_list,'SAG_trials':SAG_list,'V_aG_trial':V_aG_list})
+    else:
+        new_dict.append({'ZCT_true':ZCT_true,'E0_true':E0_true,'VAG_true':VAG_true,'V_aG_true':V_aG_true,'SAG_true':SAG_true,'ZCT_trials_based_on_E0andVAGoftrials':np.array(ZCT_all),'ZCT_err':ZCT_err,'E0_trials':E0_list,'VAG_trials':VAG_list,'SAG_trials':SAG_list,'V_aG_trial':V_aG_list})
+    zct_pkl=pd.DataFrame.from_dict(new_dict) 
+    zct_pkl.to_pickle('ResultsZCT/'+'ZCT_'+system+'_'+sample_type+'_'+density+'_'+number_trials+'_T_'+str(T_tunnel[0])+'.pkl')
+    
+    print('ResultsZCT/'+'ZCT_'+system+'_'+sample_type+'_'+density+'_'+number_trials+'_T_'+str(T_tunnel[0])+'.pkl')
+    
+    count_zct=0
+    for i in range(ntrials):
+        ratio=ZCT_all[i]/ZCT_true
+        if ratio<0.1 or ratio>10:
+            count_zct+=1
+    
+    print('#Trials with ZCT error within chemical accuracy:',ntrials-count_zct)    
+    
+    
 
-plt.figure(0)
-plt.hist(ZCT_err)
-plt.xlabel('ZCT Error (%)')
-plt.ylabel('Number of Trials')
+
+###########################################################################
 
 
-plt.savefig('ResultsZCT/'+'ZCT_hist_'+system+'_'+sample_type+'_'+density+'_'+number_trials+'_T_'+str(T_tunnel[0])+'.png')
+s_values=np.array(s)
 
-new_dict=[]
-new_dict.append({'ZCT_true':ZCT_true,'E0_true':E0_true,'VAG_true':VAG_true,'SAG_true':SAG_true,'ZCT_trials':np.array(ZCT_all),'ZCT_err':ZCT_err,'E0_trials':E0_list,'VAG_trials':VAG_list,'SAG_trials':SAG_list})
-zct_pkl=pd.DataFrame.from_dict(new_dict) 
-zct_pkl.to_pickle('ResultsZCT/'+'ZCT_'+system+'_'+sample_type+'_'+density+'_'+number_trials+'_T_'+str(T_tunnel[0])+'.pkl')
-     
+#1000 K
+G_1000K=np.array(alldf['G, VMC, 1000K'])
+G_1000K_true=G_true[-1]
+deltaG_1000K=[]
+deltaG_1000K_true=G_1000K_true-G_1000K_true[0]
+fig, ax = plt.subplots(figsize=(8,12),dpi=200)
+
+for i in range(ntrials):
+    deltaG_1000K.append(G_1000K[i]-G_1000K[i][0])
+    #ax.plot(s_values,G_1000K[i],linestyle='solid',lw=1.35)
+    ax.plot(s_values,G_1000K[i]-G_1000K[i][0],linestyle='solid',lw=1.35)
+    if i==0:
+        #ax.legend(fontsize=20)
+        ax.set_xlabel('s',fontsize=25)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        ax.set_ylabel('$\Delta G_{vib,1000K}$ (kcal/mol)',fontsize=25)
+        #ax.set_title('C2H6_C6H5_noproj'+filename.split('_')[3])
+        #ax.grid()
+        
+
+#ax.set_xlim([-0.5,0.5]) 
+#ax.set_ylim([-2.5,10]) 
+ax.plot(s_values,G_1000K_true-G_1000K_true[0],linestyle='solid',color='black',lw=3)
+#ax.plot(s_values,G_1000K_true,linestyle='solid',color='black',lw=3)
+ax.fill_between(s_values,G_1000K_true-G_1000K_true[0]-accuracy,G_1000K_true-G_1000K_true[0]+accuracy,color='grey',alpha=0.2)
+s_truedeltaG_max=s_values[(np.where(deltaG_1000K_true==max(deltaG_1000K_true)))[0][0]]
+s_deltaG_max=[]
+count_s_deltaG=0
+for i in range(ntrials):
+    s_max_trial=s_values[(np.where(deltaG_1000K[i]==max(deltaG_1000K[i])))[0][0]]
+    s_deltaG_max.append(s_max_trial)
+    if  s_max_trial==s_truedeltaG_max:
+        continue
+    count_s_deltaG+=1
+print('Percentage of trials that have the same deltaG peak:',(ntrials-count_s_deltaG)/ntrials*100)
+print('Standard deviation:',np.std(s_deltaG_max))
+print('s of max deltaG:',s_truedeltaG_max)
+
+
+#Adiabatic Ground Potential 
+
+ZPEs=np.array(alldf['ZPE, VMC'])
+VMEP=np.array(alldf['Energies'])[0]
+VMEP=np.array(VMEP)
+vag_true=(VMEP-VMEP[0])*htokcal+ZPE_true
+fig, ax = plt.subplots(figsize=(8,12),dpi=200)
+vag=[]
+
+for i in range(ntrials):
+    vag.append(ZPEs[i]+(VMEP-VMEP[0])*htokcal)
+    #ax.plot(s_values,G_500K[i],linestyle='solid',lw=1.35)
+    ax.plot(s_values,ZPEs[i]+(VMEP-VMEP[0])*htokcal,linestyle='solid',lw=1.35)
+    if i==0:
+        #ax.legend(fontsize=20)
+        ax.set_xlabel('s',fontsize=25)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        ax.set_ylabel('$V_a^G$ (kcal/mol)',fontsize=25)
+#        ax.set_title('C2H6_C6H5_noproj'+filename.split('_')[3])
+        #ax.grid()
+    
+ax.plot(s_values,(VMEP-VMEP[0])*htokcal+ZPE_true,linestyle='solid',color='black',lw=3)
+#ax.plot(s_values,G_100K_true,linestyle='solid',color='black',lw=3))
+ax.fill_between(s_values,vag_true-accuracy,vag_true+accuracy,color='grey',alpha=0.2)
+
+
+#MEP
+
+fig, ax = plt.subplots(figsize=(8,12),dpi=200)
+ax.plot(s_values,(VMEP-VMEP[0])*htokcal,linestyle='solid',color='black',lw=3)
+ax.set_xlabel('s',fontsize=25)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+ax.set_ylabel('$V_{MEP}$(kcal/mol)',fontsize=25)
+
+
+#ZPE
+fig, ax = plt.subplots(figsize=(8,12),dpi=200)
+
+
+for i in range(ntrials):
+
+    ax.plot(s_values,ZPEs[i],linestyle='solid',lw=1.35)
+    if i==0:
+        #ax.legend(fontsize=20)
+        ax.set_xlabel('s',fontsize=25)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        ax.set_ylabel('ZPE (kcal/mol)',fontsize=25)
+#        ax.set_title('C2H6_C6H5_noproj'+filename.split('_')[3])
+        #ax.grid()
+    
+ax.plot(s_values,ZPE_true,linestyle='solid',color='black',lw=3)
+#ax.plot(s_values,G_100K_true,linestyle='solid',color='black',lw=3))
+ax.fill_between(s_values,ZPE_true-accuracy,ZPE_true+accuracy,color='grey',alpha=0.2)
+
+
+s_truevag_max=s_values[(np.where(vag_true==max(vag_true)))[0][0]]
+s_vag_max=[]
+count_s=0
+for i in range(ntrials):
+    s_max_trial=s_values[(np.where(vag[i]==max(vag[i])))[0][0]]
+    s_vag_max.append(s_max_trial)
+    if  s_max_trial==s_truevag_max:
+        continue
+    count_s+=1
+print('\nPercentage of trials that have the same VaG peak:',(ntrials-count_s)/ntrials*100)
+print('Standard deviation:',np.std(s_vag_max))
+print('s of max VaG',s_truevag_max)
